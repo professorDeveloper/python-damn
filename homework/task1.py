37ec31f093ef721596dbbb2cf7317431a9041867
professorDeveloper <azamovhud007@outlook.com>
2024-10-08 18:40:03 +0500

    Homework-1 Done

---
+++ homework/task1.py
@@ -0,0 +1,10 @@
+symbol = input("Enter a symbol: ")
+
+result = f"""
+        {symbol}
+       {symbol}{symbol}{symbol}
+      {symbol}{symbol}{symbol}{symbol}{symbol}
+     {symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}
+"""
+
+print(result)
