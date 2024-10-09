e11e42ac005015fbeb704d1a80c18f1196930c8b
professorDeveloper <azamovhud007@outlook.com>
2024-10-09 18:03:10 +0500

    Day2 Task1 Success

---
+++ day2.py
@@ -0,0 +1,24 @@
+#Task1
+
+name =input ('Enter your name: ')
+
+length =len(name)
+
+print(length)
+
+
+#task2
+name =input ('Enter your name: ')
+
+name *=10
+print(name)
+
+#task3
+name =input ('Enter your name: ')
+age =input("Enter your age:")
+
+temp =name
+name =age
+age =temp
+
+print(f"Your Name : {name} ,Your Age :{age}")
