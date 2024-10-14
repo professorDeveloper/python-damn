e58dc405d91d5c955b6057b5b3a66ee845b8ea77
professorDeveloper <azamovhud007@outlook.com>
2024-10-14 18:18:06 +0500

    Day5 doing tasks

---
+++ bool_tasks/task1.py
@@ -0,0 +1,15 @@
+
+# task8
+
+a = float(input("Birinchi sonni kiriting: "))
+b = float(input("Ikkinchi sonni kiriting: "))
+
+print("Bo'lish: ", a / b)
+print("Darajaga ko'tarish: ", a ** b)
+
+# task 11
+num = int(input("Enter num: "))
+
+checkNum = num > 10
+
+print(checkNum)
--- task2.py
+++ task2.py
@@ -13,17 +13,3 @@ print(f"x//y={x // y}")
 
 print(f"x**y={x ** y}")
 
-# task8
-
-a = float(input("Birinchi sonni kiriting: "))
-b = float(input("Ikkinchi sonni kiriting: "))
-
-print("Bo'lish: ", a / b)
-print("Darajaga ko'tarish: ", a ** b)
-
-# task 11
-num = int(input("Enter num: "))
-
-checkNum = num > 10
-
-print(checkNum)
