e04c18b17c9025f9033394002462c0ce97f9d4db
professorDeveloper <azamovhud007@outlook.com>
2024-10-14 18:29:48 +0500

    Day5 doing tasks

--- bool_tasks/task1.py
+++ bool_tasks/task1.py
@@ -1,4 +1,3 @@
-
 # task8
 a = float(input("Birinchi sonni kiriting: "))
 b = float(input("Ikkinchi sonni kiriting: "))
@@ -6,11 +5,8 @@ b = float(input("Ikkinchi sonni kiriting: "))
 print("Bo'lish: ", a / b)
 print("Darajaga ko'tarish: ", a ** b)
 
-
-
 # task 11
 num = int(input("Enter num: "))
 
-checkNum = num > 10
-
-print(checkNum)
+# to`gri ya`ni true   aks holda  noto`g`ri  ya`ni false
+print(num > 10)  # asosiy ish arifmetik amal bilan  bo`lyaptil
