8148669261a8a2cddd361544663cc197d8c7adae
professorDeveloper <azamovhud007@outlook.com>
2024-10-15 11:09:49 +0500

    Learn python hem

--- bool_tasks/task1.py
+++ bool_tasks/task1.py
@@ -10,3 +10,18 @@ num = int(input("Enter num: "))
 
 # to`gri ya`ni true   aks holda  noto`g`ri  ya`ni false
 print(num > 10)  # asosiy ish arifmetik amal bilan  bo`lyaptil
+
+# HomeWork1
+num = int(input("Enter num: "))
+res = num % 2 == 0
+print(res)
+
+# Homework2
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+p = 2 * (a + b)
+s = a * b
+
+print(f"P = {p}")
+print(f"Area = {s}")
