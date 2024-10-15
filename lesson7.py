13a4b16ba3d589dc0edd265cd3a4ded0dbe3436a
professorDeveloper <azamovhud007@outlook.com>
2024-10-15 17:56:25 +0500

    Day 7 doing tasks

--- lesson7.py
+++ lesson7.py
@@ -1,3 +1,34 @@
-name = "Husan"
-name.upper()
+name = "Azamov"
+
+name = name.upper()  ## AZAMOV
+
 print(name)
+
+# Task1
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter b number: "))
+result = num1 - num2
+print(result)
+
+# task2
+isBigFirst = num1 > num2
+print(f"Birinchi son ikkinchisidan kattami ?{isBigFirst}")
+
+# task3
+
+isBigSecond = num1 <= num2
+print(f"Ikkinchi son Birinchisidan kattami yoki teng ?{isBigSecond}")
+
+# task 4
+
+resNum = num1 / num2
+print(resNum)
+
+# task5
+resNum = num1 // num2
+print(resNum)
+
+# task6
+numDiv = num1 % num2
+print(numDiv)
