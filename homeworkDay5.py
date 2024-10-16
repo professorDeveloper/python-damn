36f5eb6cf12e798f512144aa3fde305858b75b8a
professorDeveloper <azamovhud007@outlook.com>
2024-10-16 10:26:10 +0500

    Day8 homework completed

---
+++ homeworkDay5.py
@@ -0,0 +1,7 @@
+number = int(input("4 xonali son kiriting:"))
+
+birlar = number % 10
+onlar = number // 10 % 10
+yuzlar = number // 100 % 10
+minglar = number // 1000 % 10
+print(f"{minglar},{yuzlar},{onlar},{birlar}")
