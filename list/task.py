688cf85a329fc483226ccf9bc08974524df79140
professorDeveloper <azamovhud007@outlook.com>
2024-11-04 18:27:08 +0500

    Yesterday Tasks done

--- list/task.py
+++ list/task.py
@@ -9,3 +9,27 @@ for i in range(1, 11):
 print(sum)
 
 # Task3
+sum = 0
+for i in range(1, 101):
+    sum = sum + i
+print(sum)
+
+# Task4
+sum = 1
+for i in range(1, 4):
+
+    sum = sum * i
+print(f"Ko`paytiruv :{sum}")
+
+# Task5
+sum = 0
+for i in range(0, 101,2):
+    sum = sum + i
+print(sum)
+
+# Task6
+sum = 0
+for i in range(1, 101,2):
+    sum = sum + i
+print(sum)
+
