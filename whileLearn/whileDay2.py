dd17b35e63a712ce8c12d1cef26000b47bb02395
professorDeveloper <azamovhud007@outlook.com>
2024-11-12 17:27:50 +0500

    While Day2

--- whileLearn/whileDay2.py
+++ whileLearn/whileDay2.py
@@ -6,3 +6,33 @@ while numer > 0:
 
 
 print(count)
+
+
+# Task1
+i =1
+sum=0
+while i <= 100:
+    sum = sum + i
+    i = i + 1
+print(sum)
+
+# Task2
+juftSum =0
+toqSum =0
+i =0
+while i <= 50:
+    if i%2==0:
+        juftSum =juftSum +i
+    else :
+        toqSum =toqSum +i
+    i = i + 1
+print(juftSum)
+print(toqSum)
+# Task3
+i=-80
+count =0
+while i<80:
+    if i%7==0:
+        count=count+1
+    i = i + 1
+print(count)
