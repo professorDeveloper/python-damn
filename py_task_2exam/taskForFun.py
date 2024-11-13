2b5c78b7ba8faaeb41edb7859f6f42c9d2277d4b
professorDeveloper <azamovhud007@outlook.com>
2024-11-13 18:21:02 +0500

    While Day3

--- py_task_2exam/taskForFun.py
+++ py_task_2exam/taskForFun.py
@@ -1,14 +1,14 @@
 # 1 - usul   kiritilgan barcha sonlarni listga saqlash
-# 2- usul barcha belgilarni ham alohida listga +index bilan saqlash
-#
-misol = input("Misolni kiriting:")
-matrix = [[]]
-list = []
-for i in range(0, len(misol)):
-    if misol[i].isdigit():
-        list.append(int(misol[i]))
-    else:
-        matrix.append(list)
-        list.clear()
+# # 2- usul barcha belgilarni ham alohida listga +index bilan saqlash
+# #
+# misol = input("Misolni kiriting:")
+# matrix = [[]]
+# list = []
+# for i in range(0, len(misol)):
+#     if misol[i].isdigit():
+#         list.append(int(misol[i]))
+#     else:
+#         matrix.append(list)
+#         list.clear()
 
-print(matrix)
+print(int("-23"))
