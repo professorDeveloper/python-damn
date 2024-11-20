ebc522d5f6ac666dc7f7285458456af849d41254
professorDeveloper <azamovhud007@outlook.com>
2024-11-20 18:16:20 +0500

    my hobby Dict hobby :)

--- dictDay.py
+++ dictDay.py
@@ -1,7 +1,7 @@
-# Dictionary (Map) in py
-dict ={1:'one',2:'two',3:'three'}
-for e in dict:
-    print(e,dict[e],type(dict[e]))
+# # Dictionary (Map) in py
+# dict = {1: 'one', 2: 'two', 3: 'three'}
+# for e in dict:
+#     print(e, dict[e], type(dict[e]))
 
 # Dictionary app
 # {
@@ -18,3 +18,9 @@ for e in dict:
 #     totalWord:int
 # }
 #
+# Dictionary da key 1 marotaba ishlatilinadi
+
+dict = {"students": {"azamov": {"name": "Saikou", "age": 18, }, "jack": {"name": "Jack", "age": 18}}}
+for e in dict:
+    for v in dict[e]:
+        print(f"Student Name: {dict[e][v]['name']} | Age: {dict[e][v]['age']}")
