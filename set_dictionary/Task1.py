0f7b29cfc4a0987238cea5fb881f60acc607c1f8
azamov <azamovhud007@outlook.com>
2024-11-25 18:11:55 +0500

    Check strong num task done with for

--- set_dictionary/Task1.py
+++ set_dictionary/Task1.py
@@ -13,3 +13,14 @@ if fact_num==old_num:
     print(f"{old_num} Kuchli raqam")
 else:
     print(f"{old_num} Kuchli raqam emas")
+
+
+# 2-usuli with For
+fact_num2 = 0
+for i in str(old_num):
+    fact_num2+=math.factorial(int(i))
+
+if fact_num2==old_num:
+    print(f"{old_num} Kuchli raqam")
+else:
+    print(f"{old_num} Kuchli raqam emas")
