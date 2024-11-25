c34efe6ec9ab0a976f86dbe397991c6bd1a04238
professorDeveloper <azamovhud007@outlook.com>
2024-11-25 10:45:54 +0500

    Task3 Done

--- dict_week/Task3.py
+++ dict_week/Task3.py
@@ -1,1 +1,10 @@
 # Task3
+
+country = {"America": "Washington", "UK": "London", "Uzbekistan": "Tashkent", "Russian": "Moscow", "China": "Pekin",
+           "Japan": "Tokyo", "Korea": "Seoul"}
+query = input('Qaysi davlatning poytaxtini bilishni istaysiz?:')
+data = country.get(query, "-1")
+if data != '-1':
+    print(f"{query} ning poytaxti {data} ")
+else:
+    print("Kechirasiz bizda bunday davlat yo`q")
