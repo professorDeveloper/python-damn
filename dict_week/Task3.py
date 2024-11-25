71f2096eb5c70b1fd091c8e4e7ea80a27bcb98aa
professorDeveloper <azamovhud007@outlook.com>
2024-11-25 17:34:53 +0500

    Task4 Updated

--- dict_week/Task3.py
+++ dict_week/Task3.py
@@ -3,8 +3,9 @@
 country = {"America": "Washington", "UK": "London", "Uzbekistan": "Tashkent", "Russian": "Moscow", "China": "Pekin",
            "Japan": "Tokyo", "Korea": "Seoul"}
 query = input('Qaysi davlatning poytaxtini bilishni istaysiz?:')
-data = country.get(query, "-1")
-if data != '-1':
+data = country.get(query, "false")
+
+if data != 'false':
     print(f"{query} ning poytaxti {data} ")
 else:
     print("Kechirasiz bizda bunday davlat yo`q")
---
+++ sort_day/buuble_sort.py
@@ -0,0 +1,1 @@
+# Learn buble sort
