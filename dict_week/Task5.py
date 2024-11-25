feae9bf7a1ac96c3df29e09b00bbbcef92228484
professorDeveloper <azamovhud007@outlook.com>
2024-11-25 10:56:29 +0500

    Task5 Updated

--- dict_week/Task5.py
+++ dict_week/Task5.py
@@ -2,7 +2,8 @@
 map = {"id": "10001", "password": "admin12@3", "name": "Admin"}
 print("Before: ",map)
 for k, v in map.items():
-    if k == 'password' and v.find('@') == -1:
+    if k == 'password' and v.find('@') == -1: # agar mavjud bo`lsa bu shart emas )
         map[k] = f"{v}@"
+        break
 
 print("After: ",map)
