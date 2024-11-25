696251dcd0b1d8624a1de5ad49f68ca6b793a478
professorDeveloper <azamovhud007@outlook.com>
2024-11-25 10:55:00 +0500

    Task5 Done

--- dict_week/Task5.py
+++ dict_week/Task5.py
@@ -1,1 +1,8 @@
 # Task5
+map = {"id": "10001", "password": "admin123", "name": "Admin"}
+print("Before: ",map)
+for k, v in map.items():
+    if k == 'password':
+        map[k] = f"{v}@"
+
+print("After: ",map)
