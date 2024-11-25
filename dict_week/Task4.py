df5c8d3631c933c963845194b4f6c1860e8506cb
professorDeveloper <azamovhud007@outlook.com>
2024-11-25 10:52:34 +0500

    Tas4 Done

--- dict_week/Task4.py
+++ dict_week/Task4.py
@@ -1,1 +1,25 @@
 # Task4
+menu = {
+    "osh": 20000,
+    "non": 4000,
+    "sho'rva": 15000,
+    "kabob": 30000,
+    "lag'mon": 18000,
+    "manti": 25000,
+    "somsa": 7000,
+    "choy": 2000,
+    "shashlik": 15000,
+    "salat": 10000
+}
+max_foods =int(input("Buyrtmalar Soni:"))
+buyurtmalar = []
+
+while len(buyurtmalar) < max_foods:
+    taom = input(f"{len(buyurtmalar) + 1}-taomni kiriting: ").lower()
+    buyurtmalar.append(taom)
+
+for taom in buyurtmalar:
+    if taom in menu:
+        print(f"{taom} - {menu[taom]} so`m")
+    else:
+        print(f"Kechirasiz, Bizda {taom}  yo`q.")
