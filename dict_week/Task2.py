de8b0ec72852b64276941a435a5618afaa442580
professorDeveloper <azamovhud007@outlook.com>
2024-11-25 10:39:28 +0500

    Task2 done

---
+++ dict_week/Task2.py
@@ -0,0 +1,14 @@
+# Task2
+# Country & Capital Sorted
+country = {"America":"Washington","UK":"London","Uzbekistan":"Tashkent","Russian":"Moscow","China":"Pekin","Japan":"Tokyo","Korea":"Seoul"}
+capital_list = list(country.values())
+capital_list.sort()
+print("======= Capitals Sorted List =======")
+for capital_country in capital_list:
+    print(capital_country)
+country_list = list(country.keys())
+country_list.sort()
+print()
+print("======= Countries Sorted List =======")
+for country_country in country_list:
+    print(country_country)
