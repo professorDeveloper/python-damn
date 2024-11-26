dd6a12ce3f28846d7422f3517656ad8401024096
professorDeveloper <azamovhud007@outlook.com>
2024-11-26 14:42:42 +0500

    Learn Sort

--- learnSort.py
+++ learnSort.py
@@ -1,2 +1,9 @@
-mlist =[2,3,4,1,45,12,33]
-for i in range(0,len(mlist)):
+mlist = [2, 3, 4, 1, 45, 12, 33]
+for i in range(0, len(mlist)):
+    for j in range(0, len(mlist) - i - 1):
+        if mlist[j] > mlist[j + 1]:
+            mlist[j], mlist[j + 1] = mlist[j + 1], mlist[j]
+
+print(f"Sorted List:{mlist}")
+
+# Bubble sort example
