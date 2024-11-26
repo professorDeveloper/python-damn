61367ebd335634ca353725b8efc49e4921bbbdfa
professorDeveloper <azamovhud007@outlook.com>
2024-11-26 17:37:31 +0500

    Bubble sort 3 type

--- learnSort.py
+++ learnSort.py
@@ -1,9 +1,35 @@
-mlist = [2, 3, 4, 1, 45, 12, 33]
-for i in range(0, len(mlist)):
-    for j in range(0, len(mlist) - i - 1):
-        if mlist[j] > mlist[j + 1]:
-            mlist[j], mlist[j + 1] = mlist[j + 1], mlist[j]
+# Learn buble sort
 
-print(f"Sorted List:{mlist}")
+my_list = [2, 3, 4, 1, 6, 12, 33, 0, 1]
+# after 1,4
+
+for i in range(0, len(my_list)):
+    for j in range(0, len(my_list) - 1):
+        if my_list[j] > my_list[j + 1]:  # 2>3 =false,3>4=false,4>1=true
+            temp = my_list[j]
+            my_list[j] = my_list[j + 1]
+            my_list[j + 1] = temp
+
+print(my_list)
+
+# Buuble sort 2-type
+my_list = [2, 3, 4, 1, 45, 12, 33]
+
+for i in range(len(my_list)):
+    for j in range(0, len(my_list)):
+        if my_list[i] < my_list[j]:
+            temp = my_list[i]
+            my_list[i] = my_list[j]
+            my_list[j] = temp
+print(my_list)
+
+
+my_list = [2, 3, 4, 1, 45, 12, 33]
+for i in range(0, len(my_list)):
+    for j in range(0, len(my_list) - i - 1):
+        if my_list[j] > my_list[j + 1]:
+            my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+
+print(f"Sorted List:{my_list}")
 
 # Bubble sort example
