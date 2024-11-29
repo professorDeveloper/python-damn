4b711e5b9f89eaa22c27b7a951bdf19ff15a6a7a
professorDeveloper <azamovhud007@outlook.com>
2024-11-29 14:00:59 +0500

    new Task Done )

---
+++ enjoy_tasks/task2.py
@@ -0,0 +1,23 @@
+m = 5
+n = 5
+parking = []
+for i in range(m):
+    row = []
+    for j in range(n):
+        row.append("0")
+    parking.append(row)
+
+for i in range(m):
+    for j in range(n):
+        print(parking[i][j], end=" ")
+    print()
+while True:
+    row = int(input("Qator raqami: "))
+    col = int(input("Ustun raqami: "))
+    carName = input("Mashina nomi: ")
+    parking[row][col] = carName
+    print(f"{row} qator {col} ustunga {carName} mashina qoyildi")
+    for i in range(m):
+        for j in range(n):
+            print(parking[i][j], end=" ")
+        print()
