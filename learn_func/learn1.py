b96ea76ba50b775846091d94e4150e8b3ca297f5
professorDeveloper <azamovhud007@outlook.com>
2024-12-02 17:52:51 +0500

    tutor

--- learn_func/learn1.py
+++ learn_func/learn1.py
@@ -1,15 +1,27 @@
-# Before Def task1
-import random
+# # Before Def task1
+# import random
+#
+# sonlar = [1, 3, 4, 1, 66, 16, 71]
+# for i in sonlar:
+#     if i % 2 == 0:
+#         print(i)
+# # Before Def task2
+# juftList = []
+# for i in sonlar:
+#     if i % 2 == 0:
+#         juftList.append(i)
+#
+#
+def thing():
+    print("Hello World")
+    print("Fun")
 
-sonlar = [1, 3, 4, 1, 66, 16, 71]
-for i in sonlar:
-    if i % 2 == 0:
-        print(i)
-# Before Def task2
-juftList = []
-for i in sonlar:
-    if i % 2 == 0:
-        juftList.append(i)
+
+def thing2() -> str:
+    return "Hello World"
 
 
+thing()
+a = thing2()
 
+print(a)
