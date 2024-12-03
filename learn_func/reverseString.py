8711cee227aa33d8e80e391d3fe4ab3ddc830484
professorDeveloper <azamovhud007@outlook.com>
2024-12-03 18:18:49 +0500

    Reverse String II

--- learn_func/reverseString.py
+++ learn_func/reverseString.py
@@ -1,25 +1,14 @@
-# Reverse String II
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        result = []
+        n = len(s)
 
-def reverse(string, k):
-    if k != 0 and k < len(string):
-        lastArr = []
-        firstArr = []
-        lastReverse = string[len(string) - k:]
-        middle = string[k + 1:len(string) - k]
-        firstReverse = string[0:k]
-        for i in lastReverse:
-            lastArr.append(i)
-        for i in firstReverse:
-            firstArr.append(i)
-        firstArr.reverse()
-        lastArr.reverse()
-        lastReverse = ""
-        firstReverse = ""
-        for i in lastArr:
-            lastReverse += i
-        for i in firstArr:
-            firstReverse += i
-        return firstReverse + middle + lastReverse
+        # Process the string in chunks of 2k characters
+        for i in range(0, n, 2 * k):
+            # Reverse the first k characters of the current chunk
+            result.append(s[i:i+k][::-1])
+            # Append the next k characters without reversing (if any)
+            result.append(s[i+k:i+2*k])
 
-
-print(reverse("Hello World", 2))
+        # Join the result list into a string and return
+        return ''.join(result)
