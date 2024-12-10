a8572598b673f0d397401c6bca9c73606f361c9b
professorDeveloper <azamovhud007@outlook.com>
2024-12-10 18:27:47 +0500

    What if args ) task bug fixed

--- what_if/args_.py
+++ what_if/args_.py
@@ -1,16 +1,17 @@
 
-# Topshiriq 1
+# Task 1
 def checkScore(**kwargs):
     ortacha_ball = 0
-    best_subject: str = ""
-    max_score = 0
+    max_score = 5
+    good_subject ={}
     for key, value in kwargs.items():
         ortacha_ball += value
-        if value > max_score:
+        if value >= max_score:
             max_score = value
-            best_subject = key
+            good_subject[key] = value
+
     print(f"Umumiy o`rtacha Baho:{ortacha_ball // len(kwargs.values())}")
-    print(f"Eng yashi o`zlashtirgan fan: {best_subject} : {max_score}")
+    print(f"Eng yashi o`zlashtirgan fan: {good_subject}")
 
 
 checkScore(ITS=4, Matematika=3, English=4, Programming=5, Web_Development=5, Enterprise=4)
