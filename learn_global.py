fc1db6881ee999c1cf898970ac5a6109f5b062a7
professorDeveloper <azamovhud007@outlook.com>
2024-12-10 18:13:05 +0500

    what if kwargs & args

--- learn_global.py
+++ learn_global.py
@@ -22,8 +22,26 @@ kammunal_tulov()
 print(summa)
 
 
-def talabalar(*,talaba):
+def talabalar(*, talaba):
     print(talaba)
 
 
-talabalar(talaba = ("Saikou", "Azamov", "Aikou"))
+talabalar(talaba=("Saikou", "Azamov", "Aikou"))
+
+
+# ARgs qachonki parametr berish nomalum bo`lsa foydalanimz
+def lol_arg(*args):
+    print(args)
+
+
+lol_arg(2, 31, "44")
+
+
+# kwargs esa args kabi lekin dictionary type da malumot oladi asosan mening tushunishim bo`yicha
+# bu 2 funksiyadagi ish bajarish sharti paramterlar ga tasi qilmasligi kerak ekan
+
+def lol_kwarg(**kwargs):
+    print(kwargs)
+
+
+lol_kwarg(foo="bar", cat="mosh", alien="Bob")
